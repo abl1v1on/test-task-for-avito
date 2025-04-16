aa3666547d1d5ee747ce1861deec61052314067f
Maxim Danilov <abl1v1on@yandex.ru>
2025-04-16 12:13:04 +0300

    add game details page class

--- task2-2/pages/game_details_page.py
+++ task2-2/pages/game_details_page.py
@@ -9,6 +9,14 @@ from pages.base_page import BasePage
 @dataclass(frozen=True)
 class GameDetailsPageSelectors:
     GAME_NAME = (By.CSS_SELECTOR, 'h2')
+    PLATFORM = (
+        By.XPATH, 
+        '//span[text()="Platform"]/following-sibling::*[1]'
+    )
+    GENERE = (
+        By.XPATH,
+        '//span[text()="Genre"]/following-sibling::*[1]'
+    )
 
 
 class GameDetailsPage(BasePage):
@@ -16,5 +24,13 @@ class GameDetailsPage(BasePage):
         super().__init__(browser, timeout)
 
     @property
-    def game_name(self) -> WebElement:
-        return self.find(*GameDetailsPageSelectors.GAME_NAME)
+    def game_name(self) -> str:
+        return self.find(*GameDetailsPageSelectors.GAME_NAME).text
+
+    @property
+    def platform(self) -> str:
+        return self.find(*GameDetailsPageSelectors.PLATFORM).text
+
+    @property
+    def genere(self) -> str:
+        return self.find(*GameDetailsPageSelectors.GENERE).text
